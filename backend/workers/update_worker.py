2fff9364a559d8dc21d49b18ca7c6ab023fb10a2
Andro2K <kirito100crack@gmail.com>
2026-02-20 14:08:20 -0500

    Release V2.3.2

--- backend/workers/update_worker.py
+++ backend/workers/update_worker.py
@@ -12,7 +12,7 @@ from packaging import version
 # =========================================================================
 # CONFIGURACIÓN DE VERSIÓN
 # =========================================================================
-INTERNAL_VERSION = "2.3.1"
+INTERNAL_VERSION = "2.3.2"
 UPDATE_JSON_URL = "https://raw.githubusercontent.com/Andro2k/KickMonitor/refs/heads/main/version.json"
 
 # =========================================================================
