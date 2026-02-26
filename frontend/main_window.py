914197cbac88836b54f37b2d4d000b7f62b469ef
Andro2K <kirito100crack@gmail.com>
2026-02-26 17:19:38 -0500

    Release V2.5.0

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -56,7 +56,7 @@ class MainWindow(QMainWindow):
 
     def _setup_app_id(self):
         with suppress(Exception):
-            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(u'kickmonitor.v2.4.1')
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(u'kickmonitor.v2.5.0')
 
     def _init_pages(self):
         db = self.controller.db
